937d584f62f6b1ab71bf308e930ba4b9a58de411
Avishek02 <avitong9@gmail.com>
2025-12-10 21:22:29 +0600

    Return asset functionality added to the controller

--- controllers/assigned.controller.js
+++ controllers/assigned.controller.js
@@ -1,4 +1,6 @@
 const AssignedAsset = require("../models/AssignedAsset")
+const Asset = require("../models/Asset")
+const Request = require("../models/Request")
 
 const getEmployeeAssets = async (req, res) => {
   try {
@@ -10,4 +12,39 @@ const getEmployeeAssets = async (req, res) => {
   }
 }
 
-module.exports = { getEmployeeAssets }
+const returnAsset = async (req, res) => {
+  try {
+    const { id } = req.params
+    const email = req.user.email
+
+    const assigned = await AssignedAsset.findOne({ _id: id, employeeEmail: email, status: "assigned" })
+    if (!assigned) {
+      return res.status(404).json({ message: "Asset not found or already returned" })
+    }
+
+    assigned.status = "returned"
+    assigned.returnDate = new Date()
+    await assigned.save()
+
+    const asset = await Asset.findById(assigned.assetId)
+    if (asset) {
+      asset.availableQuantity += 1
+      await asset.save()
+    }
+
+    await Request.findOneAndUpdate(
+      {
+        assetId: assigned.assetId,
+        requesterEmail: email,
+        requestStatus: "approved",
+      },
+      { requestStatus: "returned" }
+    )
+
+    res.json({ message: "Asset returned successfully" })
+  } catch (error) {
+    res.status(500).json({ message: "Server error" })
+  }
+}
+
+module.exports = { getEmployeeAssets, returnAsset }
--- routes/assigned.routes.js
+++ routes/assigned.routes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const { verifyToken } = require("../middlewares/auth.middleware")
-const { getEmployeeAssets } = require("../controllers/assigned.controller")
+const { getEmployeeAssets, returnAsset } = require("../controllers/assigned.controller")
 
 const router = express.Router()
 
 router.get("/", verifyToken, getEmployeeAssets)
+router.patch("/:id/return", verifyToken, returnAsset)
 
 module.exports = router
